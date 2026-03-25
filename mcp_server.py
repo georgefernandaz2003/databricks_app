2012b8d8d6c893ccd844992f171dcf0d5abb658f
georgefernandaz2003 <george3032003@gmail.com>
2026-03-25 10:26:19 +0530

    test

---
+++ mcp_server.py
@@ -0,0 +1,15 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("sales-tools")
+
+@mcp.tool()
+def get_sales():
+    """Return sales data"""
+    return [
+        {"product": "Laptop", "sales": 120},
+        {"product": "Phone", "sales": 200},
+        {"product": "Tablet", "sales": 80}
+    ]
+
+if __name__ == "__main__":
+    mcp.run()
